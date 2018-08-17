0910ec60d886938c343d0d777f0938d8e352e454
Matheus Sousa <matheus.ads.study@gmail.com>
2018-08-17 11:24:49 -0300

    Produto e associacao muitos para muitos

---
+++ src/main/java/com/matheus/cusomc/repositories/ProdutoRepository.java
@@ -0,0 +1,9 @@
+package com.matheus.cusomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import com.matheus.cusomc.domain.Produto;
+
+@Repository
+public interface ProdutoRepository extends JpaRepository<Produto, Integer>{
+}
