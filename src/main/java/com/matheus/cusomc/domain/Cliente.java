0aa7c54c67d1ed8056fc031f6d00d2b1c3d82166
Matheus Sousa <matheus.ads.study@gmail.com>
2018-08-31 06:33:35 -0300

    Atualizacao: utilizando somente JsonIgnore

--- src/main/java/com/matheus/cusomc/domain/Cliente.java
+++ src/main/java/com/matheus/cusomc/domain/Cliente.java
@@ -14,8 +14,7 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
 
-import com.fasterxml.jackson.annotation.JsonBackReference;
-import com.fasterxml.jackson.annotation.JsonManagedReference;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.matheus.cusomc.domain.enums.TipoCliente;
 
 @Entity	
@@ -32,7 +31,6 @@ public class Cliente implements Serializable{
 	
 	//Cliente pode Serializar endereços;
 	
-	@JsonManagedReference
 	@OneToMany(mappedBy="cliente")
 	private List<Endereco> enderecos = new ArrayList<>();
 	
@@ -41,7 +39,7 @@ public class Cliente implements Serializable{
 	@CollectionTable(name="TELEFONE")
 	private Set<String> telefones = new HashSet<>();
 	
-	@JsonBackReference
+	@JsonIgnore
 	@OneToMany(mappedBy="cliente")
 	private List<Pedido> pedidos = new ArrayList<>();
 	
--- src/main/java/com/matheus/cusomc/domain/Endereco.java
+++ src/main/java/com/matheus/cusomc/domain/Endereco.java
@@ -9,8 +9,7 @@ import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 
-import com.fasterxml.jackson.annotation.JsonBackReference;
-import com.fasterxml.jackson.annotation.JsonManagedReference;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 @Entity
 public class Endereco implements Serializable{
@@ -25,12 +24,11 @@ public class Endereco implements Serializable{
 	private String bairro;
 	private String cep;
 	
-	@JsonBackReference
+	@JsonIgnore
 	@ManyToOne
 	@JoinColumn(name = "cliente_id")
     private Cliente cliente;
 	
-	@JsonManagedReference
 	@ManyToOne()
 	@JoinColumn(name = "cidade_id")
     private Cidade cidade;
