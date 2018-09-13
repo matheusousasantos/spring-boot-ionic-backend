52d902ccbbeaece044d06f98e651d66346338199
Matheus Sousa <matheus.ads.study@gmail.com>
2018-09-13 14:52:38 -0300

    Inserindo uma nova Categoria com o POST

--- src/main/java/com/matheus/cusomc/resources/CategoriaResource.java
+++ src/main/java/com/matheus/cusomc/resources/CategoriaResource.java
@@ -1,11 +1,15 @@
 package com.matheus.cusomc.resources;	
 
+import java.net.URI;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.matheus.cusomc.domain.Categoria;
 import com.matheus.cusomc.services.CategoriaService;
@@ -17,10 +21,22 @@ public class CategoriaResource {
 	@Autowired
 	private CategoriaService service;	
 	
+	//Método que retorna uma categoria através do ID passado no formato JSON pelo Postman
 	@RequestMapping(value="/{id}", method = RequestMethod.GET) //Verbos do HTTP
 	public ResponseEntity<?> find(@PathVariable Integer id) {
 		Categoria obj = service.buscar(id);
 		return ResponseEntity.ok().body(obj);
 	}
+	
+	//Método que insere uma categoria no formato JSON a partir do POST
+	@RequestMapping(method = RequestMethod.POST)
+	public ResponseEntity<Void> insert(@RequestBody Categoria obj) {
+		obj = service.insert(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId())
+				.toUri();
+		return ResponseEntity.created(uri).build();
+	}
+	
+	
 
 }
